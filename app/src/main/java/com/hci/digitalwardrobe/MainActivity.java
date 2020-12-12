aa7b243d456a3f8433b5e94beb492974dd3acef9
OsirisHS <johannes-pinzek@web.de>
2020-12-12 16:10:48 +0100

    Implemented basic Weather-API for live weather data. TODO: get weather data that predicts weather.

--- app/src/main/java/com/hci/digitalwardrobe/MainActivity.java
+++ app/src/main/java/com/hci/digitalwardrobe/MainActivity.java
@@ -2,14 +2,32 @@ package com.hci.digitalwardrobe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureWeatherButton();
     }
 
     @Override
@@ -17,4 +35,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void configureWeatherButton() {
+        Button nextButton = (Button) findViewById(R.id.ButtonWeather);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/hci/digitalwardrobe/TestActivity.java
+++ app/src/main/java/com/hci/digitalwardrobe/TestActivity.java
@@ -2,13 +2,83 @@ package com.hci.digitalwardrobe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 public class TestActivity extends AppCompatActivity {
+
+    Weather weather;
+    TextView cityName;
+    Button searchButton;
+    TextView result;
+
+    public void search(View view){
+        cityName = findViewById(R.id.cityName);
+        searchButton = findViewById(R.id.search);
+        result = findViewById(R.id.result);
+
+        String cName = cityName.getText().toString();
+
+        String content;
+        weather = new Weather();
+        try {
+            content =  weather.execute("https://api.openweathermap.org/data/2.5/weather?q="+ cName + "&appid=299e88b027bee5dcfe55b4566f1064b0").get();
+
+            //JSON
+            JSONObject jsonObject = new JSONObject(content);
+            String weatherData = jsonObject.getString("weather");
+            String mainTemperature = jsonObject.getString("main");
+            JSONArray array = new JSONArray(weatherData);
+
+            String main = "";
+            String description = "";
+            String temperature = "";
+
+            for(int i = 0;i<array.length();i++){
+                JSONObject weatherpart = array.getJSONObject(i);
+                main = weatherpart.getString("main");
+
+                description = weatherpart.getString("description");
+            }
+
+            JSONObject mainPart = new JSONObject(mainTemperature);
+            temperature = mainPart.getString("temp");
+            float temperatureInt = Float.parseFloat(temperature);
+            temperatureInt -= 273.15;
+            temperature = Float.toString(temperatureInt);
+
+            Log.i("main", main);
+            Log.i("description", description);
+
+            String resultText = "Main : "+ main +
+                    "\nDescription : " + description +
+                    "\nTemperature : " + temperature;
+
+            result.setText(resultText);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
     }
+
+
 }
